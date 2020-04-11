baa4ff9196e8e62c9c8e52455c93592156e48535
Jan296929B <jan@berchtold-web.com>
2020-04-11 17:35:18 +0200

    adds a second thread; in the next step a queue for each station will be added

--- AufgabeEins/SupermarketRealzeitsimulation.py
+++ AufgabeEins/SupermarketRealzeitsimulation.py
@@ -5,34 +5,48 @@ import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s] (%(threadName)-1s) %(message)s')
 
-event = threading.Event()
-arrEv = threading.Event()
 CUSTOMER_TIME = 10
 STATION_TIME = 5
+event = threading.Event()
+arrEv = threading.Event()
+lock_station = threading.Lock()
+lock_customer = threading.Lock()
+
+queue = []
 
 
 def customer_thread():
+    lock_customer.acquire()
     event.set()
     if arrEv.wait(timeout=CUSTOMER_TIME):
         logging.debug('buyed grocerys')
         arrEv.clear()
+        lock_customer.release()
         return
+    lock_customer.release()
     arrEv.clear()
     logging.debug('the queue is to long >.<')
 
 
 def station_thread():
-    event.wait()
-    time.sleep(STATION_TIME)
-    logging.debug('finished with station')
-    arrEv.set()
-    event.clear()
+    while True:
+        event.wait()
+        print("acquire lock")
+        lock_station.acquire()
+        print("lock acquired")
+        time.sleep(STATION_TIME)
+        logging.debug('finished with station')
+        arrEv.set()
+        lock_station.release()
+        event.clear()
 
 
 def main():
     t1 = threading.Thread(name='customer', target=customer_thread)
+    t3 = threading.Thread(name='customer', target=customer_thread)
     t2 = threading.Thread(name='station', target=station_thread)
     t1.start()
+    t3.start()
     t2.start()
 
 
